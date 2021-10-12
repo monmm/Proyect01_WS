baf5b7f81267d4e86f68da329eb7493309ff57da
monica <monmm@ciencias.unam.mx>
2021-10-11 23:21:58 -0500

    Añadí documentación

--- tickets.py
+++ tickets.py
@@ -4,7 +4,7 @@ from imprime import respuesta
 
 class Ticket:
     
-    api_key = "2b0278a7ed481d119f4ffb3a34a6d97d" # api_key de monica
+    api_key = "2b0278a7ed481d119f4ffb3a34a6d97d" # api_key de @monica
     sol_key = Solicita(api_key) # inicializamos nuestra llave para OpenWeather
     bdd = Datos("dataset1.csv") # definimos nuestra base de datos
     Datos.setData(bdd) # obtenemos los datos de nuestra base de datos
@@ -14,11 +14,46 @@ class Ticket:
     num_ticket = 1 
 
     def getDesCoord(self):
+        """Obtiene las coordenas de la ciudad de destino.
+
+        Devuelve en una tupla la latitud y longitud de 
+        la ciudad de destino.
+
+        Utiliza los datos del ticket -- self -- para obtener la 
+        información.
+    
+        """
         return (self.get('destination_latitude'), self.get('destination_longitude'))
 
     def getOrCoord(self):
+        """Obtiene las coordenas de la ciudad de origen.
+
+        Devuelve en una tupla la latitud y longitud de 
+        la ciudad de origen.
+
+        Utiliza los datos del ticket -- self -- para obtener la 
+        información.
+    
+        """
         return (self.get('origin_latitude'), self.get('origin_longitude'))
 
+    """Realizamos la petición para cada ticket de nuestra lista de tickets.
+
+    Verificamos que o hayamos hecho ya la solicitud para 
+    las ciudades de origen o destino.
+
+    Si no hemos realizado la petición,
+        obtenemos las coordenadas y solicitamos la petición,
+        guardando la respuesta de la API.
+        Finalmente, añadimos la respuesta con el nombre de la ciudad 
+        en el diccionario de peticiones.
+    Si ya realizamos la petición,
+        obtenemos la respuesta a la solicitud de 
+        nuestra lista de peticiones realizadas.
+
+    Por último, mandamos a imprimir la información solicitada de nuestro ticket.
+
+    """
     for ticket in bdd.tickets:
         if ticket.get('origin') not in peticiones.keys():
             coord = getOrCoord(ticket)
@@ -36,5 +71,6 @@ class Ticket:
 
         respuesta(num_ticket, or_answer, des_answer)
         num_ticket += 1 
-    # Verificamos la cantidad de peticiones realizadas a OpenWeather
-    print(len(peticiones))
+    
+    # Código para verificar el número de peticiones realizadas a OpenWeather.
+    # print(len(peticiones))
