ac6698f04ee3e0a29c99c87ff328eb5e90b264f3
monica <monmm@ciencias.unam.mx>
2021-10-24 15:48:33 -0500

    Actualización de los test de peticiones

--- src/test/Test_peticiones.py
+++ src/test/Test_peticiones.py
@@ -13,12 +13,31 @@ class TestSolicita(unittest.TestCase):
     lon =  -100.107
 
     def test_city_request(self):
+        """Test para las peticiones por ciudad.
+
+        Verificamos que se lance la excepción correspondiente 
+        con un código de ciudad incorrecto.
+
+        Excepciones a verificar:
+        TypeError -- cuando el valor no es una cadena
+        ValueError -- cuando el código no es de 3 carateres
+    
+        """
         self.assertRaises(TypeError,Solicita.cityRequest,self,10)
         with self.assertRaises(ValueError) as exception_info:
             Solicita.cityRequest(self,"MILAN", (0,0))
         assert str(exception_info.exception) == 'Código de ciudad incorrecto'
 
     def test_coord_request(self):
+        """Test para las peticiones por coordenadas.
+
+        Verificamos que se lance la excepción correspondiente 
+        cuando nuestras coordenadas no son correctas.
+
+        Excepciones a verificar:
+        TypeError -- cuando el valor de la longitud o la latitud 
+        no son una cadena    
+        """
         self.assertEquals (self.lat,self.v['coord']['lat']) 
         self.assertEquals (self.lon,self.v['coord']['lon']) 
         self.assertRaises(TypeError,Solicita.cityRequest,10)
